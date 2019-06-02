f9cd5f188e7d738dbf12a53f449e48737adefa0c
Fatty <fatty.arbuckle.87@gmail.com>
2019-06-02 01:31:53 -0400

    Tweaks project meta data

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
-defmodule NervesSystemRpi3.MixProject do
+defmodule AdsbNervesRpi3.MixProject do
   use Mix.Project
 
-  @app :nerves_system_rpi3
+  @app :adsb_nerves_rpi3
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
            |> String.trim()
@@ -35,7 +35,7 @@ defmodule NervesSystemRpi3.MixProject do
     [
       type: :system,
       artifact_sites: [
-        {:github_releases, "nerves-project/#{@app}"}
+        {:github_releases, "fatty-arbuckle/#{@app}"}
       ],
       build_runner_opts: build_runner_opts(),
       platform: Nerves.System.BR,
@@ -67,7 +67,7 @@ defmodule NervesSystemRpi3.MixProject do
       maintainers: ["Frank Hunleth", "Justin Schneck"],
       files: package_files(),
       licenses: ["Apache 2.0"],
-      links: %{"GitHub" => "https://github.com/nerves-project/#{@app}"}
+      links: %{"GitHub" => "https://github.com/fatty-arbuckle/#{@app}"}
     ]
   end
 
